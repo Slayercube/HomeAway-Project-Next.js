80384b09871ef9821f638d1e6ff6bbab47d2bad9
deepak <sidhudeep9292@gmail.com>
2024-10-21 21:22:26 +0100

    setup image action

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,13 @@
-import { LuUser2 } from 'react-icons/lu';
+import { LuUser2 } from "react-icons/lu";
+import { fetchProfileImage } from "@/utils/actions";
 
-function UserIcon() {
-  return <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white' />;
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+  if (profileImage) {
+    return (<img src={profileImage} className="w-6 h-6 object-cover rounded-full" />
+    )
+  }
+
+  return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 export default UserIcon;
