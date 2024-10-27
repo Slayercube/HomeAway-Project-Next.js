4b4bcd7cffaeb7eaa2ae74da58f866d87f725086
deepak <sidhudeep9292@gmail.com>
2024-10-27 18:34:18 +0000

    setup loading property in cards

--- components/card/PropertyCard.tsx
+++ components/card/PropertyCard.tsx
@@ -38,10 +38,14 @@ function PropertyCard({ property }: { property: PropertyCardProps }) {
             night
           </p>
           {/* country and flag */}
+          <CountryFlagAndName countryCode={country} />
         </div>
       </Link>
       <div className='absolute top-5 right-5 z-5'>
         {/* favorite toggle button */}
+        <div className='absolute top-5 right-5 z-5'>
+  <FavoriteToggleButton propertyId={propertyId} />
+</div>
       </div>
     </article>
   );
